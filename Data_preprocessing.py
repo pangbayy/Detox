26d74d04133e5f4ab84ade810b1ad6f7137b5076
Bei <jiayu.pang@mail.utoronto.ca>
2020-01-14 20:49:02 -0500

    Import libraries need

---
+++ Data_preprocessing.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import nltk
+from bs4 import BeautifulSoup
+import string
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem import WordNetLemmatizer
+from nltk.stem.porter import PorterStemmer
+
+df = pd.read_csv('train.csv')
+
+
+def main():
+    print(df.shape)
