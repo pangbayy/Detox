e962330b79caaec7b3bc3dc900ee20644bb0bb4d
Sherry Wang <sherrysw.wang@mail.utoronto.ca>
2020-02-15 14:46:07 -0500

    modified

--- function.py
+++ function.py
@@ -1,15 +1,18 @@
-import pandas as pd
-import numpy as np
-import nltk
-nltk.download('wordnet')
-from bs4 import BeautifulSoup
 import string
+
+import nltk
+import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
-from nltk.tokenize import RegexpTokenizer
 from nltk.stem import WordNetLemmatizer
 from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import RegexpTokenizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+
+nltk.download('wordnet')
+
 
 def remove_html(text):
     soup = BeautifulSoup(text, 'lxml')
@@ -29,18 +32,23 @@ def remove_stopwords(text):
     words = [w for w in text if w not in stopwords.words('english')]
     return words
 
+
 # Instantiate lemmatizer
 lemmatizer = WordNetLemmatizer()
+
 
 def word_lemmatizer(text):
     lem_text = [lemmatizer.lemmatize(i) for i in text]
     return lem_text
 
+
 # Instantiate Stemmer
 stemmer = PorterStemmer()
 
+
 def word_stemmer(text):
-    stem_text = " ".join([stemmer.stem(i) for i in text])
+    # stem_text = " ".join([stemmer.stem(i) for i in text])
+    stem_text = [" ".join([stemmer.stem(i) for i in text])]
     return stem_text
 
 
@@ -50,12 +58,14 @@ def call_all():
     print(df.shape)
 
     # remove punctuation
-    df['comment_text'] = df['comment_text'].apply(lambda x: remove_punctuation(x))
+    df['comment_text'] = df['comment_text'].apply(
+        lambda x: remove_punctuation(x))
 
     # Instantiate Tokenizer
     tokenizer = RegexpTokenizer('\w+|\$[\d\.]+|\S+')
 
-    df['comment_text'] = df['comment_text'].apply(lambda x: tokenizer.tokenize(x.lower()))
+    df['comment_text'] = df['comment_text'].apply(
+        lambda x: tokenizer.tokenize(x.lower()))
     # print(df['comment_text'].head(20))
 
     # remove stop words
@@ -67,37 +77,54 @@ def call_all():
     # print(df['comment_text'].apply(lambda x: word_lemmatizer(x)))
     # print(df['comment_text'].apply(lambda x: word_stemmer(x)))
 
-    df['comment_text'] = df['comment_text'].apply(lambda x: word_lemmatizer(x))
+    # df['comment_text'] = df['comment_text'].apply(lambda x: word_lemmatizer(x))
+
+    df['comment_text'] = df['comment_text'].apply(lambda x: word_stemmer(x))
 
     print(df['comment_text'])
 
-# make every word into numeric vectors, called the bag of words model. Display it as a table
-clean_text = np.vectorize(call_all)
-cv = CountVectorizer(min_df=0., max_df=1.)
-cv_matrix = cv.fit_transform(clean_text)
-cv_matrix = cv_matrix.toarray()
-print(cv_matrix)
-# get all unique words in the corpus
-vocab = cv.get_feature_names()
-# show document feature vectors
-pd.DataFrame(cv_matrix, columns=vocab)
+    return df['comment_text']
 
 
-# creat a bigram model to see the frequency of phrases
-bv = CountVectorizer(ngram_range=(2,2))
-bv_matrix = bv.fit_transform(clean_text)
-
-bv_matrix = bv_matrix.toarray()
-vocab = bv.get_feature_names()
-pd.DataFrame(bv_matrix, columns=vocab)
-
-
-#tfidf model of the words
-tv = TfidfVectorizer(min_df=0., max_df=1., use_idf=True)
-tv_matrix = tv.fit_transform(norm_corpus)
-tv_matrix = tv_matrix.toarray()
-
-vocab = tv.get_feature_names()
-pd.DataFrame(np.round(tv_matrix, 2), columns=vocab)
 if __name__ == '__main__':
+    corpus = []
 
+    for i in call_all():
+        corpus.extend(i)
+
+    norm_corpus = np.array(corpus)
+
+    print(norm_corpus)
+
+    # Bag of Words Model
+    # make every word into numeric vectors, called the bag of words model.
+    # Display it as a table
+    cv = CountVectorizer(min_df=0., max_df=1.)
+    cv_matrix = cv.fit_transform(norm_corpus)
+    cv_matrix = cv_matrix.toarray()
+    print(cv_matrix)
+
+    # get all unique words in the corpus
+    vocab = cv.get_feature_names()
+    print(vocab)
+    # show document feature vectors
+    print(pd.DataFrame(cv_matrix, columns=vocab))
+
+    # Bag of N-Grams Model
+    # create a bigram model to see the frequency of phrases
+    bv = CountVectorizer(ngram_range=(2, 2))
+    bv_matrix = bv.fit_transform(norm_corpus)
+
+    bv_matrix = bv_matrix.toarray()
+    vocab = bv.get_feature_names()
+    print(vocab)
+    print(pd.DataFrame(bv_matrix, columns=vocab))
+
+    # TF-IDF(Term Frequency-Inverse Document Frequency) Model
+    tv = TfidfVectorizer(min_df=0., max_df=1., use_idf=True)
+    tv_matrix = tv.fit_transform(norm_corpus)
+    tv_matrix = tv_matrix.toarray()
+
+    vocab = tv.get_feature_names()
+    print(vocab)
+    print(pd.DataFrame(np.round(tv_matrix, 2), columns=vocab))
